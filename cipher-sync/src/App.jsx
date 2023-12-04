04122d69687eba7859489ef61f2cdf1723d4ac4a
SparrowH <christophernyakotey@gmail.com>
2023-12-04 16:06:15 +0000

    initial commit

---
+++ cipher-sync/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import './App.css'
+const App = () => {
+  return (
+    <div className='app__background'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App  
---
+++ cipher-sync/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='cipher__navbar'>
+        <div />
+        <div className='cipher__navbar-links'>
+          <ul>
+            <li><a href="#About">About</a></li> 
+            <li><a href="#FAQ">FAQ</a></li>
+          </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
